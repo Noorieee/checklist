616924a01570d30250cd562534b3614e7ae3b746
John <goralight@gmail.com>
2025-02-16 10:48:58 +0000

    init

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>❤️🦦 Hi Noory 🦦❤️</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
