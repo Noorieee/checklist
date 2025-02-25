4095ce1abb166edc405a120a64c6e6e07fd50a8b
Noor <hussainmahnoor112@gmail.com>
2025-02-25 11:59:44 +0000

    created input and button and storing list items

--- src/components/Checklist/index.tsx
+++ src/components/Checklist/index.tsx
@@ -1,26 +1,28 @@
 import React from "react"
 import ChecklistItem, { ChecklistItemProps } from "../ChecklistItem"
-
+import { useState } from "react";
 
 const Checklist = () => {
-    const list: ChecklistItemProps[] = [
-        {
-            label: "hello",
-            checked: true,
-        },
-        {
-            label: "sgffdgd",
-            checked: true,
-        },
-        {
-            label: "test",
-            checked: false,
-        }
-    ]
+    const [list, setList] = useState<ChecklistItemProps[]>([]);
+    const [inputValue, setInputValue] = useState("");
+    const addItem = () => {
+        if (inputValue === "") return
+
+        setList([
+            ...list, 
+            {
+                label: inputValue, 
+                checked: false
+            }   
+        ])
+        setInputValue("")
+    }
 
     return (
         <div>
             <h1>hello</h1>
+            <input value={inputValue} onChange={(e) => {setInputValue(e.target.value)}} type="text"/>
+            <button onClick={addItem}>Add</button>
             {list.map((item) => (
                 <ChecklistItem label={item.label} checked={item.checked}/>
             ))}
