f8a773598def10ed14a6e1bd783a0b9d8830ccdc
Noor <hussainmahnoor112@gmail.com>
2025-03-13 17:09:44 +0000

    added a delete item button and added a checklist item on enter

--- src/components/Checklist/index.tsx
+++ src/components/Checklist/index.tsx
@@ -5,8 +5,10 @@ import { useState } from "react";
 const Checklist = () => {
     const [list, setList] = useState<ChecklistItemProps[]>([]);
     const [inputValue, setInputValue] = useState("");
-    const addItem = () => {
-        if (inputValue === "") return
+    const addItem = ():void => {
+        if (inputValue === "") {
+            return
+        } 
 
         setList([
             ...list, 
@@ -18,13 +20,29 @@ const Checklist = () => {
         setInputValue("")
     }
 
+    const removeItem = (index: number):void => {
+        const firstHalf = list.slice(0, index)
+        const secondHalf = list.slice(index + 1)
+        const newList = [...firstHalf, ...secondHalf]
+        setList(newList)
+    }
+
+    const handleOnKeyDown = (event:React.KeyboardEvent<HTMLInputElement>) => {
+        if (event.key === 'Enter') {
+            addItem()
+        }
+    }
+
     return (
         <div>
             <h1>hello</h1>
-            <input value={inputValue} onChange={(e) => {setInputValue(e.target.value)}} type="text"/>
-            <button onClick={addItem}>Add</button>
-            {list.map((item) => (
-                <ChecklistItem label={item.label} checked={item.checked}/>
+            <input value={inputValue} onKeyDown={(event) => handleOnKeyDown(event)} onChange={(event) => {setInputValue(event.target.value)}} type="text"/>
+            <button onClick={() => {addItem()}}>Add</button>
+            {list.map((item, index) => (
+                <div key={`${item.label}-${index}`}>
+                    <ChecklistItem label={item.label} checked={item.checked}/>
+                    <button onClick={() => {removeItem(index)}}>Delete</button>
+                </div>    
             ))}
         </div>
     )
